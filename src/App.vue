25db0c18c0eeda54a0388944892c5a1f94733e78
AnaelBonnafous <anael.bonnafous@gmail.com>
2023-07-05 14:57:17 +0200

    feat(routing): install vue router and create layout

--- src/App.vue
+++ src/App.vue
@@ -1,115 +1,31 @@
-<script setup lang="ts">
-import {computed, onMounted, ref} from 'vue'
-
-import {RoverWithState} from './rover/roverWithState.ts'
-import {Point} from './geometrie/point'
-import {Position} from './geometrie/position'
-import {Entier} from "./math/Entier.ts"
-import {Orientations} from './topologie/orientations'
-import {PlanèteAvecObstacles} from './topologie/planeteAvecObstacles.ts'
-import {PlanèteToroïdaleVide} from './topologie/planeteToroïdale.ts'
-import {InterpréteurRover} from './rover/interpréteurRover.js'
-import {CommandeSimple} from './rover/commande/CommandeSimple.ts'
-
-const planet = new PlanèteToroïdaleVide(new Entier(8))
-const planeteAvecObstacles = new PlanèteAvecObstacles(planet)
-planeteAvecObstacles.AjouterObstacle(1, 1)
-planeteAvecObstacles.AjouterObstacle(2, 2)
-planeteAvecObstacles.AjouterObstacle(7, 1)
-const positionAtterrissage = new Position(new Point(new Entier(5), new Entier(2)), planeteAvecObstacles)
-
-const rover = ref(new RoverWithState(Orientations.Sud, positionAtterrissage))
-const intepreteurRover = ref(new InterpréteurRover(rover.value))
-
-const executeCommand = (command: string) => {
-    const data = intepreteurRover.value.Interpréter(new CommandeSimple(command))
-    rover.value = data._rover
-    intepreteurRover.value = data
-}
-
-onMounted(() => {
-    document.addEventListener('keydown', (ev: KeyboardEvent) => {
-        switch (ev.key) {
-            case "z":
-            case "ArrowUp":
-                executeCommand("A")
-                break;
-            case "q":
-            case "ArrowLeft":
-                executeCommand("G")
-                break;
-            case "s":
-            case "ArrowDown":
-                executeCommand("R")
-                break;
-            case "d":
-            case "ArrowRight":
-                executeCommand("D")
-                break;
-            default:
-                break;
-        }
-    })
-})
-
-const isObstacle = (i: number, j: number): boolean => !planeteAvecObstacles.EstAccessible(new Point(new Entier(i), new Entier(j)))
-
-const latitudes = Array.from(Array(planeteAvecObstacles._decorated._pointMax._latitude._valeur), (_, index) => index).slice().reverse()
-const longitudes = Array.from(Array(planeteAvecObstacles._decorated._pointMax._longitude._valeur), (_, index) => index)
-
-const roverLatitude = computed(() => rover.value.Position._point._latitude._valeur)
-const roverLongitude = computed(() => rover.value.Position._point._longitude._valeur)
-</script>
-
 <template>
-    <div id="game">
-        <h1>Opération MALT69775643</h1>
-        <table id="planet">
-            <tbody>
-            <tr v-for="i in latitudes">
-                <td v-for="j in longitudes">
-                    <template
-                        v-if="rover && (i === roverLatitude && j === roverLongitude)">
-                        <div id="robot">🤖</div>
-                    </template>
-                    <template v-else-if="isObstacle(i, j)">
-                        <div class="obstacle">🪨</div>
-                    </template>
-                    <template v-else>
-                        <div class="ground">&nbsp;</div>
-                    </template>
-                </td>
-            </tr>
-            </tbody>
-        </table>
-        <span id="message">Atterissage : succès le 05 Juillet 2023 à 12h27 heure FR</span>
+    <div id="layout">
+        <nav>
+            <router-link to="/">
+                <button>Rover</button>
+            </router-link>
+            <router-link to="/console">
+                <button>Console</button>
+            </router-link>
+        </nav>
+        <router-view></router-view>
+        <footer>
+            Made with ❤️ and 🤖 by Mathieu NEYRET, Anaël BONNAFOUS, Logan LE SAUX and Théo NICOLAS
+        </footer>
     </div>
 </template>
 
 <style scoped>
-#game {
+#layout {
     height: 100vh;
     display: flex;
     flex-direction: column;
-    justify-content: center;
+    justify-content: space-between;
     align-items: center;
 }
 
-#planet td {
-    width: 30px;
-    height: 30px;
-    border: 1px solid #fff;
-    text-align: center;
-/*    padding: 0;*/
-}
-
-
-#robot {
-    transform: rotate(v-bind(rover.Orientation.toDegree() + "deg"));
-}
-
-#message {
-    margin-top: 1em;
-    font-style: italic;
+#layout nav {
+    display: flex;
+    gap: 1em;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,19 @@
 import './style.css'
 
-import { createApp } from 'vue'
+import {createApp} from 'vue'
+import {createRouter, createWebHistory} from 'vue-router'
 import App from './App.vue'
+import Rover from './Rover.vue'
+import Console from './Console.vue'
 
-createApp(App).mount('#app')
+const routes = [
+    {path: '/', component: Rover},
+    {path: '/console', component: Console},
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+})
+
+createApp(App).use(router).mount('#app')
